192e5490fbc3ac32fe6c86f9b49ec7a615b9462f
Nekipel <88710417+Nekipel@users.noreply.github.com>
2022-04-28 17:32:08 +0300

    feat:add new function common-character

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let count = 0
+  let arrNew =[]
+  s1=s1.split("").sort((a,b)=>a-b)
+  //a,a,b,c,c->a,a,b,c,c->[a,a,b,c,c]
+  s2=s2.split("").sort((a,b)=>a-b)
+  //a,d,c,a,a->a,a,c,d->[a,a,c,d]
+  for(let i=0;i<s1.length;i++){
+    if(!s1.length){
+      break
+    }
+    for(let j=0;j<s2.length;j++){
+      if(s1[i]===s2[j]){
+        arrNew.push(s1[i]);
+        // s1[i]="0"
+        s2[j] ="0"
+        break
+      }
+    }
+  }
+  return count =arrNew.length
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
+function transform(/*arr*/) {
+//   let newArr
+//   try {
+//     arr
+//   } catch (e) {
+//     if (!Array.isArray(arr)) {
+//       "\'arr\' parameter must be an instance of the Array!"
+//     }
+//     // ... и т.д.
+//   }
+//   let answer = ['--discard-next','--double-next','--discard-next','--double-next']
+//   // // let newArr
+//   // if(!Array.isArray(arr)){
+//   //   return "\'arr\' parameter must be an instance of the Array!"
+//   //   // return "Invalid date!"
+//   // }
+// return newArr=arr.filter(
+//   x=>{
+//   if(x === answer[0]){
+//     x+1 = 
+//   }
+//   })
+
+  // for(let i=0;i<arr.length;i++){
+  //   arr.map(x=>{
+  //     if(2){
+
+  //     }
+  //   })
+  // }
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
